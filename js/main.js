0b84457965a810436771c2e84b0bb2c576e05a33
Oleg-Lucenko <lucenko5577@gmail.com>
2023-02-18 13:37:40 +0200

    HW 19

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ let dataLink2 = linkBtn2.getAttribute('data-link');
 
 
 linkBtn1.addEventListener('click', () => {
-    if (dataLink1.indexOf('http://') != 0 && dataLink1.indexOf('https://' != 0)) {
+    if (dataLink1.indexOf('http://') != 0 && dataLink1.indexOf('https://') != 0) {
         window.location.href = 'http://' + dataLink1;
     } else {
         window.location.href = dataLink1;
@@ -15,7 +15,7 @@ linkBtn1.addEventListener('click', () => {
 })
 
 linkBtn2.addEventListener('click', () => {
-        if (dataLink2.indexOf('http://') != 0 && dataLink2.indexOf('https://' != 0)) {
+        if (dataLink2.indexOf('http://') != 0 && dataLink2.indexOf('https://') != 0) {
             window.location.href = 'http://' + dataLink2;
         } else {
             window.location.href = dataLink2;
